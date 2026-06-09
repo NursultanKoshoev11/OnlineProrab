b9a3b1df40d1dc18b2efc8e157d5016f96859db4
Nursultan <102341503+NursultanKoshoev11@users.noreply.github.com>
2026-06-09 22:19:01 +0600

    Open database on API startup

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -1,16 +1,21 @@
 package main
 
 import (
-	"log"
-	"net/http"
+    "context"
+    "log"
+    "net/http"
 
-	"github.com/NursultanKoshoev11/OnlineProrab/backend/internal/config"
-	"github.com/NursultanKoshoev11/OnlineProrab/backend/internal/httpapi"
+    "github.com/NursultanKoshoev11/OnlineProrab/backend/internal/config"
+    "github.com/NursultanKoshoev11/OnlineProrab/backend/internal/database"
+    "github.com/NursultanKoshoev11/OnlineProrab/backend/internal/httpapi"
 )
 
 func main() {
-	cfg := config.Load()
-	handler := httpapi.NewRouter()
-	log.Println("OnlineProrab API listening", cfg.HTTPAddr)
-	log.Fatal(http.ListenAndServe(cfg.HTTPAddr, handler))
-}
+    cfg := config.Load()
+    db, err := database.Open(context.Background(), cfg.DatabaseURL)
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer db.Close()
+    log.Println("OnlineProrab API listening", cfg.HTTPAddr)
+    log.Fatal(http.ListenAndServe(cfg.HTTPAddr
