69bf749691ce144138fea73bf5f332ad222ea059
Nursultan <102341503+NursultanKoshoev11@users.noreply.github.com>
2026-06-09 16:18:15 +0600

    Use mobile app shell from main

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,19 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:onlineprorab/app/app.dart';
 
 void main() {
   runApp(const OnlineProrabApp());
-}
-
-class OnlineProrabApp extends StatelessWidget {
-  const OnlineProrabApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: Center(child: Text('Online Prorab MVP')),
-      ),
-    );
-  }
 }
