0c9de703571f7e50a116d0438b9f8111af7b7986
Nursultan <102341503+NursultanKoshoev11@users.noreply.github.com>
2026-06-09 16:24:56 +0600

    Use standalone Flutter app shell

--- mobile/lib/app/app.dart
+++ mobile/lib/app/app.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:online_prorab/app/theme.dart';
-import 'package:online_prorab/features/dashboard/dashboard_screen.dart';
 
 class OnlineProrabApp extends StatelessWidget {
   const OnlineProrabApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Online Prorab',
-      theme: AppTheme
+      home: Scaffold(
+        body: Center(child: Text('Online Prorab MVP')),
+      ),
+    );
+  }
+}
