dced97f91adce89589e25872fc5ebf049c84ecbc
Nursultan <102341503+NursultanKoshoev11@users.noreply.github.com>
2026-06-10 13:12:42 +0600

    Route legacy server entrypoint through production app startup

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -1,14 +1,7 @@
 package main
 
-import (
-	"log"
-	"net/http"
-
-	"github.com/NursultanKoshoev11/OnlineProrab/backend/internal/config"
-	"github.com/NursultanKoshoev11/OnlineProrab/backend/internal/httpapi"
-)
+import "github.com/NursultanKoshoev11/OnlineProrab/backend/internal/app"
 
 func main() {
-	cfg := config.Load()
-	log.Fatal(http.ListenAndServe(cfg.HTTPAddr, httpapi.NewRouter()))
+	app.Run()
 }
