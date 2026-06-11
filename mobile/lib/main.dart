64771283ed20f0085a801e528c0b2b019babf1b0
Nursultan <102341503+NursultanKoshoev11@users.noreply.github.com>
2026-06-11 12:27:45 +0600

    Initialize Flutter bindings before secure storage

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -2,5 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:online_prorab/app/backend_app_v3.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const BackendOnlineProrabAppV3());
 }
